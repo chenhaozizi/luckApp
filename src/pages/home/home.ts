90d8b99bd697ed28a2ee1ca45f4cfa1b861e9d13
chenhaozizi <845115433@qq.com>
2018-11-30 18:06:08 +0800

    ---

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { IonicPage,NavController } from 'ionic-angular';
+
+
+
+
 
 @Component({
   selector: 'page-home',
@@ -8,7 +12,7 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
-
+   
   }
 
 }
