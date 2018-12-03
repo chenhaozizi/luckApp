c7d140652df43d61e9b2b712f19419d2e2023fa1
chenhaozizi <845115433@qq.com>
2018-12-03 18:04:17 +0800

    揭晓记录状态页

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -10,9 +10,20 @@ import { IonicPage,NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  record_info_box=false;
 
   constructor(public navCtrl: NavController) {
+    
    
+  }
+
+  showRecordInfoBox(){
+    console.log('打开揭晓记录')
+    this.record_info_box=true;
+  }
+  hideRecordInfoBox(){
+    console.log('关闭揭晓记录')
+    this.record_info_box=false;
   }
 
 }
