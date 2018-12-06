0a993f71461bc1f5ac8f5231a6cb6d8f71206051
chenhaozizi <845115433@qq.com>
2018-12-06 15:07:50 +0800

    动态

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -6,9 +6,20 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'contact.html'
 })
 export class ContactPage {
-
+  select=0;
+  tabs = [];
   constructor(public navCtrl: NavController) {
+    this.tabs=[
+      { key: '最新参与记录', id: 0, isSelect: true },
+      { key: '最新获奖记录', id: 1, isSelect: false },
+      { key: '揭晓记录',     id: 2, isSelect: false }
+    ]
 
   }
-
+  checktab(index: number) {
+    console.log("index" + index);
+    this.tabs.forEach(tab => {tab.isSelect=false});
+    this.tabs[index].isSelect=true;
+    this.select=index;
+  }
 }
